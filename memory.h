a8530720ee911ef9f6babd988ca167b7f1931fe1
jdspell <spelljd15@gmail.com>
2021-08-28 12:15:04 -0400

    completed initial implementation for array size management macros

--- memory.h
+++ memory.h
@@ -3,10 +3,14 @@
 
 #include "common.h"
 
+// arrays will all be allocated at least 8 bytes
+// if an arrays contents outgrow it then double the array capacity
 #define GROW_CAPACITY(size) (size < 8 ? 8 : size * 2)
 
-#define GROW_ARRAY(array) ()
+// adjust the array size by passing in the desired capacity
+#define ADJUST_ARRAY(type, array, size) ((type *)reallocate(array, size))
 
+// allocating size 0 will free the array
 #define FREE_ARRAY(array, size) (reallocate(array, 0))
 
 void *reallocate(void *pointer, size_t size);
