bce23ef7f2ead7656b165656bc6694914e77083b
jdspell <spelljd15@gmail.com>
2021-08-28 14:36:40 -0400

    added check in main for array tests for array implementation

--- array.test.c
+++ array.test.c
@@ -1,2 +1,10 @@
+#include <stdio.h>
+
 #include "array.test.h"
 #include "array.h"
+
+void testArrayImpl()
+{
+    printf("BEGIN ARRAY IMPLEMENTATION TESTS\n");
+    printf("END ARRAY IMPLEMENTATION TESTS\n");
+}
--- array.test.h
+++ array.test.h
@@ -1,4 +1,16 @@
 #ifndef nnc_array_test_h
 #define nnc_array_test_h
 
+// meta test where all other tests are called from
+void testArrayImpl();
+
+// test for array initialization
+void testArrayInit();
+// test for growing the array
+void testAdjustArraySmaller();
+// test for shrinking the array
+void testAdjustArrayBigger();
+// test for freeing the array
+void testFreeArray();
+
 #endif
